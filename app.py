7a0f159303849f4f9380a7f4fe60eecae419b406
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-25 16:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-#Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
 os.system("git clone https://lazyindu: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@github.com/lazyindu/killerBOTnew okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
